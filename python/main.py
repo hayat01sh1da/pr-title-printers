9c84bbc324a90b812ef8fbf6c25fc72541539741
Hayato Ishida <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-10-19 22:11:12 +0900

    Delete __pycache__/ after execution

--- python/main.py
+++ python/main.py
@@ -1,6 +1,14 @@
 import sys
+import os
+import shutil
+import glob
 sys.path.append('./src')
 from application import Application
 
 _, branch_name, *_ = sys.argv
 print(Application(branch_name).run())
+
+pycaches = glob.glob(os.path.join('.', '**', '__pycache__'), recursive = True)
+for pycache in pycaches:
+    if os.path.isdir(pycache):
+        shutil.rmtree(pycache)
