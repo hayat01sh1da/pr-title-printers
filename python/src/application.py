1d847a474bc6e8f38057eaf2e5fde90007a64d35
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-07-23 01:15:51 +0900

    Replace str#format with f-string

--- python/src/application.py
+++ python/src/application.py
@@ -7,17 +7,17 @@ class Application:
     def run(self):
         string_array = re.split(r'\/', self.branch_name)
         *prefixes, branch_topic = string_array[-3:] if string_array[2] == 'hotfix' else string_array[-2:]
-        return '{labels} {pr_topic}'.format(labels = self.__labels__(prefixes), pr_topic = self.__pr_topic__(branch_topic))
+        return f'{self.__labels__(prefixes)} {self.__pr_topic__(branch_topic)}'
 
     # private
 
     def __labels__(self, prefixes):
         if 'hotfix' in prefixes:
-            prefixes[0] = '[{hotfix}]'.format(hotfix = prefixes[0].capitalize())
-            prefixes[-1] = '[{service}]'.format(service = prefixes[-1])
+            prefixes[0] = f'[{prefixes[0].capitalize()}]'
+            prefixes[-1] = f'[{prefixes[-1]}]'
             return ''.join(prefixes)
         else:
-            prefixes[0] = '[{service}]'.format(service = prefixes[0])
+            prefixes[0] = f'[{prefixes[0]}]'
             return ''.join(prefixes)
 
     def __pr_topic__(self, branch_topic):
