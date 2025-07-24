c1c3fbee887cdddf17f129e8cdb1bac81ddbbc70
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-07-24 16:29:29 +0900

    Switch expected values with actual values in unit tests

--- ruby/test/application_test.rb
+++ ruby/test/application_test.rb
@@ -4,11 +4,11 @@ require_relative '../src/application'
 class ApplicationTest < Minitest::Test
   def test_run
     branch_name = 'hayat01sh1da/issue-xxxxx/service/this_is_a_test_branch'
-    assert_equal(Application.run(branch_name:), '[service] This Is A Test Branch')
+    assert_equal('[service] This Is A Test Branch', Application.run(branch_name:))
   end
 
   def test_run_in_the_case_of_hotfix
     branch_name = 'hayat01sh1da/issue-xxxxx/hotfix/service/this_is_a_test_branch'
-    assert_equal(Application.run(branch_name:), '[Hotfix][service] This Is A Test Branch')
+    assert_equal('[Hotfix][service] This Is A Test Branch', Application.run(branch_name:))
   end
 end
