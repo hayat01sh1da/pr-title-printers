6526c526faf1d6d1c3290671f919b0e6245a8094
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-12-31 01:44:03 +0900

    Explicitly declare parameter names

--- python/test/test_application.py
+++ python/test/test_application.py
@@ -6,11 +6,11 @@ from application import Application
 class TestApplication(unittest.TestCase):
     def test_run(self):
         branch_name = 'hayat01sh1da/issue-xxxxx/service/this_is_a_test_branch'
-        self.assertEqual(Application(branch_name).run(), '[service] This Is A Test Branch')
+        self.assertEqual(Application(branch_name = branch_name).run(), '[service] This Is A Test Branch')
 
     def test_run_in_the_case_of_hotfix(self):
         branch_name = 'hayat01sh1da/issue-xxxxx/hotfix/service/this_is_a_test_branch'
-        self.assertEqual(Application(branch_name).run(), '[Hotfix][service] This Is A Test Branch')
+        self.assertEqual(Application(branch_name = branch_name).run(), '[Hotfix][service] This Is A Test Branch')
 
 if __name__ == '__main__':
     unittest.main()
