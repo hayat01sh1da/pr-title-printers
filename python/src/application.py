217bbd96e66434294f32e4099fa112fa1db1a601
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-04-12 01:13:54 +0900

    Introduce type checking to existing Python files

--- python/src/application.py
+++ python/src/application.py
@@ -1,17 +1,17 @@
 import re
 
 class Application:
-    def __init__(self, branch_name):
-        self.branch_name = branch_name
+    def __init__(self, branch_name: str) -> None:
+        self.branch_name: str = branch_name
 
-    def run(self):
+    def run(self) -> str:
         string_array = re.split(r'\/', self.branch_name)
         *prefixes, branch_topic = string_array[-3:] if string_array[2] == 'hotfix' else string_array[-2:]
         return f'{self.__labels__(prefixes)} {self.__pr_topic__(branch_topic)}'
 
     # private
 
-    def __labels__(self, prefixes):
+    def __labels__(self, prefixes: list[str]) -> str:
         if 'hotfix' in prefixes:
             prefixes[0] = f'[{prefixes[0].capitalize()}]'
             prefixes[-1] = f'[{prefixes[-1]}]'
@@ -20,8 +20,8 @@ class Application:
             prefixes[0] = f'[{prefixes[0]}]'
             return ''.join(prefixes)
 
-    def __pr_topic__(self, branch_topic):
-        _branch_topic = []
+    def __pr_topic__(self, branch_topic: str) -> str:
+        _branch_topic: list[str] = []
         for word in re.split(r'[\-\_]', branch_topic):
             _branch_topic.append(word.capitalize())
         return ' '.join(_branch_topic)
--- python/test/test_application.py
+++ python/test/test_application.py
@@ -4,11 +4,11 @@ sys.path.append('./src')
 from application import Application
 
 class TestApplication(unittest.TestCase):
-    def test_run(self):
+    def test_run(self) -> None:
         branch_name = 'hayat01sh1da/issue-xxxxx/service/this_is_a_test_branch'
         self.assertEqual(Application(branch_name = branch_name).run(), '[service] This Is A Test Branch')
 
-    def test_run_in_the_case_of_hotfix(self):
+    def test_run_in_the_case_of_hotfix(self) -> None:
         branch_name = 'hayat01sh1da/issue-xxxxx/hotfix/service/this_is_a_test_branch'
         self.assertEqual(Application(branch_name = branch_name).run(), '[Hotfix][service] This Is A Test Branch')
 
